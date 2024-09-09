4832e791ea13e6a01643d9019fc7e363db582208
Rafaeldfb <dfbrafa@hotmail.com>
2024-09-09 01:36:05 -0300

    home - data refactored

---
+++ buzzvel-microsite/src/data/about-me.js
@@ -0,0 +1,31 @@
+import hello from '@/../public/images/about-me-hello.jpg';
+import introduction from '@/../public/images/about-me-introduction.jpg';
+import achievements from '@/../public/images/about-me-achievements.jpg';
+import cta from '@/../public/images/about-me-cta.jpg';
+
+export const aboutMeContent = {
+  hello: {
+    title: "Connect with a Complete Full Stack Developer",
+    content: "I’m Rafael Bonatti, a full stack developer passionate about creating innovative and scalable web solutions. With over 3 years of experience, I have worked on challenging projects from conception to implementation, always striving to deliver exceptional results.",
+    imgSrc: "images/about-me-hello.jpg",
+    img: hello,
+  },
+  introduction: {
+    title: "Building the Future of the Web, One Line of Code at a Time",
+    content: "My journey as a developer has led me to master various technologies such as React.js, Node.js, Python, and PHP. Over the years, I have had the opportunity to lead complex projects and contribute to the growth of startups and established companies.",
+    imgSrc: "images/about-me-intro.jpg",
+    img: introduction,
+  },
+  achievements: {
+    title: "Tangible Results, Impactful Projects",
+    content: "Throughout my career, I’ve had the pleasure of achieving significant results, such as increasing e-commerce sales by over 200% and leading the development of essential features on global platforms. I am driven by challenges and always aim to deliver solutions that exceed expectations.",
+    imgSrc: "images/about-me-achivements.jpg",
+    img: achievements,
+  },
+  cta: {
+    title: "Let's Build the Future Together",
+    content: "I'm eager to collaborate with the talented team at Buzzvel. My skills and experience make me a perfect fit for your organization. Click below to explore and learn more about how we can work together.",
+    imgSrc: "images/about-me-cta.jpg",
+    img: cta,
+  },
+}
